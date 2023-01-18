ad967d9400d01a2fe066dcd12a1e8cf8c6745323
Saransh Shankar <saransh.shankar@gmail.com>
2023-01-18 16:05:34 +0000

    Added feature to update time onClick.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  const [state, setState] = React.useState(time);
+  function updateTime() {
+    let newTime = new Date().toLocaleTimeString();
+    setState(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{state}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
